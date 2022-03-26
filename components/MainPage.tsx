5045d3e5662ca20ee9e3eaec43d18c550fc0fd7c
Sodolsky <mateuszsodolskiofficial@gmail.com>
2022-03-26 17:11:00 +0100

    Removed unused Imports

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   addCookie,
