829bc9bc8a1191a38f7f5dcf05967b4654e20b32
Sodolsky <mateuszsodolskiofficial@gmail.com>
2022-05-08 18:19:46 +0200

    Added cool animation for cookie

--- components/clickerElements/CookieToClick.tsx
+++ components/clickerElements/CookieToClick.tsx
@@ -3,12 +3,20 @@ import { addCookie } from "../../redux/gameLogicReducer";
 import Image from "next/image";
 import CoockieImage from "../../public/cookie.png";
 import { RootState } from "../../redux/store";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDoubleClickUpgrade } from "../../utils/hooks/useDoubleClickUpgrade";
+import { animated, config, Controller, useSpring } from "react-spring";
 // export interface CookieToClickProps {
 //   setCookieCount: React.Dispatch<React.SetStateAction<number>>;
 // }
+const animationObject = {
+  from: { scale: 1 },
+  to: [{ scale: 0.8 }, { scale: 1 }],
+  config: config.wobbly,
+};
 export const CookieToClick: React.FC = () => {
+  const [isSmaller, setisSmaller] = useState<boolean>(false);
+  const [styles, api] = useSpring(() => animationObject);
   const dispatch = useDispatch();
   const CPC = useSelector(
     (state: RootState) => state.gameLogic.cookiesLogic.CPC
@@ -18,12 +26,18 @@ export const CookieToClick: React.FC = () => {
     dispatch(addCookie(CPC * multiplier));
   };
   return (
-    <Image
-      src={CoockieImage}
-      className={"cursor-pointer transition-all"}
-      onClick={handleClickIncrementation}
-      height={256}
-      width={256}
-    />
+    <animated.div style={styles}>
+      <Image
+        src={CoockieImage}
+        className={"cursor-pointer transition-all"}
+        onClick={() => {
+          handleClickIncrementation();
+          setisSmaller((prev) => !prev);
+          api.start(animationObject);
+        }}
+        height={256}
+        width={256}
+      />
+    </animated.div>
   );
 };
