d602b8c69e109ca6dc5f9cea40af82462bd2520c
Sodolsky <mateuszsodolskiofficial@gmail.com>
2022-08-01 21:21:09 +0200

    Changed to HTML semantic tags

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
     <>
       <header className="text-xl lg:text-3xl mt-4 flex gap-2 justify-center">
         <h1>Cookie Clicker Rebuild</h1>
-        <div>{isEqualibrumBought && <EqualibrumStacksDisplay />}</div>
+        <section>{isEqualibrumBought && <EqualibrumStacksDisplay />}</section>
       </header>
     </>
   );
