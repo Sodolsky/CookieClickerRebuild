83c28896158e2e3c28d89ea49cfdea227d0df79b
Sodolsky <mateuszsodolskiofficial@gmail.com>
2022-10-17 13:10:01 +0200

    Added game state loading from firebase

---
+++ utils/hooks/useAuthStatus.ts
@@ -0,0 +1,12 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../../firebase";
+
+type authStatusTypes = "loading" | "ready";
+export const useAuthStatus = () => {
+  const [authStatus, setAuthStatus] = useState<authStatusTypes>("loading");
+  auth.onAuthStateChanged((userData) => {
+    setAuthStatus("ready");
+  });
+  return { authStatus, auth };
+};
