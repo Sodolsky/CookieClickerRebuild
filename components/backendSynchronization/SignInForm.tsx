35d083573c1d5a4e2e6fb7467dff04fa4bfb204e
Sodolsky <mateuszsodolskiofficial@gmail.com>
2022-11-25 22:33:29 +0100

    Commented out unnecesary code

--- components/backendSynchronization/SignInForm.tsx
+++ components/backendSynchronization/SignInForm.tsx
@@ -37,10 +37,10 @@ export const saveUserDocumentInDatabase = async (
   await setDoc(ref, firebaseObj, { merge: true });
 };
 //Todo Add saving user utilities in database
-export const saveUserUtilsInDatabase = async (
-  email: string,
-  utilityObject: utilityObject
-) => {};
+// export const saveUserUtilsInDatabase = async (
+//   email: string,
+//   utilityObject: utilityObject
+// ) => {};
 export const SignInForm: React.FC<SignInFormInterface> = ({
   setShowSignIn,
 }) => {
