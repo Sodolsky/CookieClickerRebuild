edb74036c3e064e5ba7f8c8d1e6df5825d7f7584
Sodolsky <mateuszsodolskiofficial@gmail.com>
2023-03-14 15:22:51 +0100

    Added playing video to endgame view

--- components/skillTree/EndgameView.tsx
+++ components/skillTree/EndgameView.tsx
@@ -1,7 +1,18 @@
+import { useRef, useEffect } from "react";
+
 export const EndgameView = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.loop = true;
+      videoRef.current.play();
+    }
+  }, [videoRef]);
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-black text-white">
-      test
+    <div className="h-screen w-screen bg-black">
+      <video ref={videoRef} className="h-full w-full">
+        <source src="/path/to/video.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 };
