7e281a20963503afcc5458c88d9db18f9f4bdd50
Sodolsky <mateuszsodolskiofficial@gmail.com>
2023-03-20 16:18:20 +0100

    Added fade out animation to galaxy video

--- components/skillTree/EndgameView.tsx
+++ components/skillTree/EndgameView.tsx
@@ -1,7 +1,14 @@
-import { useRef, useEffect } from "react";
-
+import { useRef, useEffect, useState } from "react";
+interface animationObject {
+  fadeOutGalaxy: boolean;
+}
+const baseAnimationObject: animationObject = {
+  fadeOutGalaxy: false,
+};
 export const EndgameView = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [animationObject, setAnimationObject] =
+    useState<animationObject>(baseAnimationObject);
   useEffect(() => {
     if (videoRef.current) {
       videoRef.current.play();
@@ -11,8 +18,12 @@ export const EndgameView = () => {
     <div className="h-screen w-screen bg-black">
       <video
         ref={videoRef}
-        className="h-full w-full"
-        onEnded={() => console.log("video ended")}
+        className={`h-full w-full opacity-100 duration-500 ${
+          animationObject.fadeOutGalaxy && "opacity-0"
+        } transition-opacity`}
+        onEnded={() =>
+          setAnimationObject((prev) => ({ ...prev, fadeOutGalaxy: true }))
+        }
       >
         <source src="galaxyVideo.mp4" type="video/mp4" />
       </video>
