5a9a2f7ca07ec5807a307caeb675f49d706d2548
Sodolsky <mateuszsodolskiofficial@gmail.com>
2023-03-21 20:50:08 +0100

    Added fade in eternal animation

--- components/skillTree/EndgameView.tsx
+++ components/skillTree/EndgameView.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import EternalImage from "../../public/eternalone.jpg";
 import { useRef, useEffect, useState } from "react";
 interface animationObject {
   fadeOutGalaxy: boolean;
+  fadeInEternal: boolean;
 }
 const baseAnimationObject: animationObject = {
   fadeOutGalaxy: false,
+  fadeInEternal: true,
 };
 export const EndgameView = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -18,7 +22,7 @@ export const EndgameView = () => {
     <div className="h-screen w-screen bg-black">
       <video
         ref={videoRef}
-        className={`h-full w-full opacity-100 duration-500 ${
+        className={`h-full w-full opacity-100 duration-500 z-0 ${
           animationObject.fadeOutGalaxy && "opacity-0"
         } transition-opacity`}
         onEnded={() =>
@@ -26,6 +30,14 @@ export const EndgameView = () => {
         }
       >
         <source src="galaxyVideo.mp4" type="video/mp4" />
+        <Image
+          src={EternalImage}
+          alt={"Image of The Eternal"}
+          className={`opacity-0 z-10 ${
+            animationObject.fadeInEternal && "opacity-100 duration-500"
+          } `}
+          layout={"fill"}
+        />
       </video>
     </div>
   );
